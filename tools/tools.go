591d8a71735804ff72c8f6613885d8f4e6de8f86
dengqi <dengqi935@gmail.com>
2025-03-15 19:25:09 +0800

    chore: update go mod tidy

--- tools/tools.go
+++ tools/tools.go
@@ -8,16 +8,16 @@ package tools
 // 这些导入仅用于 go mod tidy 不会移除这些依赖
 
 import (
+	// 用于导入格式化
+	_ "github.com/daixiang0/gci"
 	// 用于检查循环复杂度
 	_ "github.com/fzipp/gocyclo/cmd/gocyclo"
 	// 用于代码质量检查
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
-	// 用于格式化代码
-	_ "mvdan.cc/gofumpt"
-	// 用于导入格式化
-	_ "github.com/daixiang0/gci"
 	// 用于导入格式化
 	_ "golang.org/x/tools/cmd/goimports"
 	// 用于静态代码分析
 	_ "honnef.co/go/tools/cmd/staticcheck"
+	// 用于格式化代码
+	_ "mvdan.cc/gofumpt"
 )
